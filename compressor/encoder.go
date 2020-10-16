306cc5f49097ed100b4215fd2dd14759e4d50821
Anoop S <anoop.skumar1507@gmail.com>
2020-10-16 11:52:20 +0530

    encoder: efficient way to add downscale parameter

--- compressor/encoder.go
+++ compressor/encoder.go
@@ -10,14 +10,6 @@ import (
 
 const opusBitrate = "64k"
 
-// Insert downscaling params
-func insertScaleParams(encodeCmd []string) []string {
-
-	scaleParams := []string{"-vf", "scale=1280:720"}
-
-	return append(encodeCmd[:6], append(scaleParams, encodeCmd[6:]...)...)
-}
-
 // Compress - main encoding function
 func Compress(filesToEncode []string, crf string) {
 
@@ -32,17 +24,17 @@ func Compress(filesToEncode []string, crf string) {
 
 		mainParams := []string{"-loglevel", "quiet", "-stats", "-y", "-i", "Input/" + f}
 
+		if checkRes(f) {
+			// insert downscaling param
+			color.Warn.Println("Downscaling to  720p")
+			mainParams = append(mainParams, "-vf", "scale=1280:720")
+		}
+
 		videoParams := []string{"-c:v", "libx264", "-preset", "slow", "-crf", crf, "-r", "25", "-x264-params", "ref=6:qpmin=10:qpmax=51:me=umh:bframes=6"}
 		audioParams := []string{"-c:a", "libopus", "-b:a", opusBitrate, "-vbr", "on", "-compression_level", "10", "-frame_duration", "60", "-application", "audio", "-strict", "-2", "Ouput/" + f}
 
 		encodeCmd := append(mainParams, videoParams...)
 		encodeCmd = append(encodeCmd, audioParams...)
-
-		if checkRes(f) {
-			// insert downscaling param
-			color.Warn.Println("Downscaling to  720p")
-			encodeCmd = insertScaleParams(encodeCmd)
-		}
 
 		cmd := exec.Command(ffmpeg, encodeCmd...)
 
