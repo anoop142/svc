8a863e938e6d7f7add9095cf871387c15aa463ed
Anoop S <anoop.skumar1507@gmail.com>
2020-10-23 12:16:06 +0530

    choose_crf : Allow float values of crf

--- compressor/choose_crf.go
+++ compressor/choose_crf.go
@@ -2,21 +2,18 @@ package compressor
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/gookit/color"
 )
 
 //ChooseCRF displays a dialog to choose CRF value
 func ChooseCRF() string {
-	crf := 32
+	crf := 32.0
 
 	fmt.Println()
 	color.Note.Printf("Choose a CRF value between 28 and 35 or empty for default value(%v)\n", crf)
 	color.Question.Printf("CRF value :")
+	fmt.Scanf("%f", &crf)
 
-	fmt.Scanf("%d", &crf)
-
-	return strconv.Itoa(crf)
-
+	return fmt.Sprintf("%f", crf)
 }
