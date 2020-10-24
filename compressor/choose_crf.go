0de4f0cc04c921f70877a1d236dbb12cb9a71a54
Anoop S <anoop.skumar1507@gmail.com>
2020-10-24 20:30:19 +0530

    choose_crf: crf value make it string
    we are not checking input crf value
    unlike selection number. so use plain string input.

--- compressor/choose_crf.go
+++ compressor/choose_crf.go
@@ -8,12 +8,12 @@ import (
 
 //ChooseCRF displays a dialog to choose CRF value
 func ChooseCRF() string {
-	crf := 32.0
-
+	crf := "32.0"
 	fmt.Println()
+
 	color.Note.Printf("Choose a CRF value between 28 and 35 or empty for default value(%v)\n", crf)
 	color.Question.Printf("CRF value :")
-	fmt.Scanf("%f", &crf)
 
-	return fmt.Sprintf("%f", crf)
+	fmt.Scanf("%s", &crf)
+	return crf
 }
